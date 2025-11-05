22d1a8137822e2728cf2e1f9517ff7f440552838
Michaeldlt1 <mbm.torre@yavirac.edu.ec>
2025-11-05 08:06:00 -0500

    front

---
+++ src/app/admin/inventario/components/producto/producto.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productos',
+  standalone: false,
+  templateUrl: './productos.html',
+  styleUrl: './productos.scss',
+})
+export class Productos {
+
+}
